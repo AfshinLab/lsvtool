c1b328aec7a339e6add23c8213036b46be168cc5
Humam.Siga <humam.siga@gmail.com>
2021-03-05 18:42:58 +0100

    adding igv batches for two files

--- reformat.bash
+++ reformat.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-perc=0.7
+perc=0.8
 tt="DEL" #"DEL"
 
 for file in $(ls *calls.bedpe)
@@ -20,3 +20,10 @@ Rscript plot.R -i ${perc}_${tt}_naibrs_merged.vcf -t $tt
 
 rm *log
 
+
+#if two files only use:
+less ${perc}_${tt}_naibrs_merged_list.bedpe | cut -f 1,2,5,12 | grep "1 0\|1 1" | sed 's/1 0 /first/g'  | sed 's/1 1 /both/g' > All_from_1st.bedpe
+less ${perc}_${tt}_naibrs_merged_list.bedpe | cut -f 1,2,5,12 | grep "0 1\|1 1" | sed 's/0 1 /second/g' | sed 's/1 1 /both/g' > All_from_2nd.bedpe
+
+bedtools igv -i All_from_1st.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > all_1st.batch
+bedtools igv -i All_from_2nd.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > all_2nd.batch
