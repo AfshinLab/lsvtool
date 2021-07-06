b31fd59862f4b0106dfe9eab69a830f916f3232b
Humam Siga <46369083+HSiga@users.noreply.github.com>
2021-07-06 18:58:30 +0200

    Update reformat.bash
    generate batch files for unique and common

--- reformat.bash
+++ reformat.bash
@@ -16,16 +16,18 @@ done
 
 ls $out_dir/*filtered_sorted_merged.vcf > $out_dir/files_to_merge.txt
 
-SURVIVOR merge $out_dir/files_to_merge.txt 5000 1 0 0 0 10000 $out_dir/${perc}_${tt}_naibrs_merged.vcf
+SURVIVOR merge $out_dir/files_to_merge.txt 5000 1 0 0 0 10000 $out_dir/${perc}_${tt}_SVs_merged.vcf
 
-Rscript plot.R -i $out_dir/${perc}_${tt}_naibrs_merged.vcf -t $tt 
+Rscript plot.R -i $out_dir/${perc}_${tt}_SVs_merged.vcf -t $tt 
 
 rm $out_dir/*log
 
 
 #if two files only use:
-less $out_dir/${perc}_${tt}_naibrs_merged_list.bedpe | cut -f 1,2,5,12 | grep "1 0\|1 1" | sed 's/1 0 /first/g'  | sed 's/1 1 /both/g' > $out_dir/All_from_1st.bedpe
-less $out_dir/${perc}_${tt}_naibrs_merged_list.bedpe | cut -f 1,2,5,12 | grep "0 1\|1 1" | sed 's/0 1 /second/g' | sed 's/1 1 /both/g' > $out_dir/All_from_2nd.bedpe
+less $out_dir/${perc}_${tt}_SVs_merged_list.bedpe | cut -f 1,2,5,12 | grep "1 0" | sed 's/1 0 /first/g'   > $out_dir/unique_1st.bedpe
+less $out_dir/${perc}_${tt}_SVs_merged_list.bedpe | cut -f 1,2,5,12 | grep "0 1" | sed 's/0 1 /second/g'  > $out_dir/unique_2nd.bedpe
+less $out_dir/${perc}_${tt}_SVs_merged_list.bedpe | cut -f 1,2,5,12 | grep "1 1" | sed 's/1 1 /both/g'    > $out_dir/common_both.bedpe
 
-bedtools igv -i $out_dir/All_from_1st.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > $out_dir/All_1st.batch
-bedtools igv -i $out_dir/All_from_2nd.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > $out_dir/All_2nd.batch
+bedtools igv -i $out_dir/unique_1st.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > $out_dir/unique_1st.batch
+bedtools igv -i $out_dir/unique_2nd.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > $out_dir/unique_2nd.batch
+bedtools igv -i $out_dir/common_both.bedpe -slop 1000 -clps -name | sed 's/collapse/squish/g' > $out_dir/common_both.batch
