7ffcc7be5130f04b2b80394bfacdc604bdf850b5
Humam.Siga <humam.siga@gmail.com>
2021-11-26 12:31:36 +0100

    isdigit should be with str

--- src/lsvtool/cli/filter_lsv.py
+++ src/lsvtool/cli/filter_lsv.py
@@ -49,7 +49,7 @@ def check_ext(filename):
 # naibr filter functionls
 def filter_naibr(df,quantile):
     cuttoff=df.quality.quantile(q=quantile)
-    if cuttoff.isdigit:
+    if str(cuttoff).isdigit():
         print("The top ",round((1-quantile)*100), "% of the QC will be kept", sep="")
         print("Cut off set: ",cuttoff,sep="")
         df = df[df.quality >= cuttoff]
